78b7f21c5f41e806baaf7f2c4863e3d02b615da2
Chantera Lazard <chanteralazard@Chanteras-MacBook-Air.local>
2026-01-23 15:43:54 -0600

    WIP: Add disease counter and variant parsing logic

--- project01.py
+++ project01.py
@@ -1,52 +1,81 @@
 #!/usr/bin/env python
 from pprint import pprint
+from collections import Counter
 
 
 # Modify this function signature and fill in the details
-def parse_line()
-    pass
+def parse_line(line: str) -> list:
+    """
+    Provides a list of associated diseases for rare variant in a vcf file
+    :param line: String representing a VCF variant line
+    :return: List of associated diseases for rare variant
+    """
 
+    # Initialize variables
+    variant_info_dict = {}      # Info dictionary
+    associated_diseases = []    # Associated Diseases List
+    rare_threshold = 0.0001     # Rare disease threshold based on allele frequency
 
-###Sample code below
-# Initialize string with sample data for temporary testing
-#readin = "1	1041648	263158	G	T	.	.	AF_ESP=0.00500;AF_EXAC=0.00488;AF_TGP=0.00240;ALLELEID=249308;CLNDISDB=MedGen:C3808739,OMIM:615120|MedGen:CN169374|MedGen:CN517202;CLNDN=Myasthenic_syndrome,_congenital,_8|not_specified|not_provided;CLNHGVS=NC_000001.11:g.1041648G>T;CLNREVSTAT=criteria_provided,_multiple_submitters,_no_conflicts;CLNSIG=Benign/Likely_benign;CLNVC=single_nucleotide_variant;CLNVCSO=SO:0001483;GENEINFO=AGRN:375790;MC=SO:0001583|missense_variant;ORIGIN=1;RS=138031468"
+    # VCF variants lines are made up of tab-separated columns
+    # As we are interested in the INFO column (last column)
+    # split line into columns and then grab info column
+    variant_cols = line.split("\t")
+    variant_info = variant_cols[-1]
 
-# Split data into list
-#txt = readin
-#x = txt.split(";")
-#print(x)
+    # key-value pairs in the info column are separated by ";"
+    # Create a list object that holds key-value pairs
+    variant_info_pairs = variant_info.split(";")
 
-# Create dict from list
-#my_dict = {item.split('=')[0]: item.split('=')[1] for item in x}
+    # Create a dictionary from list
+    for pair in variant_info_pairs:
+        variant_info_dict[pair.split("=")[0]] = pair.split("=")[1]
 
-# Just prints out the dictionary entries for easy viewing
-#print(my_dict)
-#for key, value in my_dict.items():
-#    print(key, "-", value)
-
-# Checks if allele frequency is at threshold
-#float(my_dict["AF_EXAC"]) < 0.0001
-
-# Split the diseases into its own string
-#diseases = my_dict["CLNDN"]
-#diseases
-
-# Pipe delimiter separates into list entries
-#disease_list = diseases.split("|")
-#disease_list
-
-# Add items to our list to pass back to calling function, removing not specified and not provided 
-#to_add = []
-#for item in disease_list:
-#    if item != "not_specified" and item != "not_provided":
-#        to_add.append(item)
-#print(to_add)
-
-
+    # Check if dictionary has allele frequency key
+    if "AF_EXAC" in variant_info_dict:
+        # Convert allele frequency value into float
+        AF_EXAC = float(variant_info_dict["AF_EXAC"])
+        # Check if variant is rare using allele frequency
+        if(AF_EXAC < rare_threshold):
+            # Get all diseases for rare variant
+            diseases = variant_info_dict["CLNDN"].split("|")
+            # Append to associated diseases list excluding not_provided and not_specified
+            for disease in diseases:
+                if disease not in ("not_provided","not_specified"):
+                    associated_diseases.append(disease)
+            # Return Associated Diseases
+            return associated_diseases
+    else:
+        # If allele frequencies does not indicate variant rareness or if not provided, return empty list
+        return []
 
 # Modify this function signature and fill in the details
-def read_file()
-    pass
+def read_file(filename: str) -> Counter:
+    """
+    Reads a file and returns disease counts
+    :param filename: String representing File path
+    :return: Dictionary mapping diseases to their counts in VCF file
+    """
+    # Initialize Counter dictionary
+    disease_counter = Counter()
+    # Try to open file, or catch exception
+    try:
+        with open(filename, "r") as infile:
+            # Read file line by line (not using readlines() as vcf files can be large)
+            for line in infile:
+                # Strip line of leading and trailing whitespace characters
+                line = line.strip()
+                # Skip lines that start with #
+                if line.startswith("#"):
+                    continue
+                # Pass line to parse_line
+                disease_list = parse_line(line)
+                # Count the results in disease list:
+                if disease_list:
+                    disease_counter.update(disease_list)
+        # Return dictionary
+        return(disease_counter)
+    except Exception as e:
+        print(f"An error occurred while reading file: {e}")
 
 
 if __name__ == "__main__":
